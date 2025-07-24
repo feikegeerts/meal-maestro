a1ddd0d29e0103fdf81205bf6ec7760da0767df8
Feike Geerts <feike.geerts@storiogroup.com>
2025-07-24 21:15:21 +0200

    refactor: simplify Vite configuration by removing unused environment variables and settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, loadEnv } from 'vite';
-import pkg from './package.json';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [sveltekit()],
-    define: {
-      __APP_VERSION__: JSON.stringify(pkg.version),
-      'process.env.SUPABASE_URL': JSON.stringify(env.SUPABASE_URL),
-      'process.env.SUPABASE_ANON_KEY': JSON.stringify(env.SUPABASE_ANON_KEY),
-    },
-    server: {
-      fs: {
-        allow: ['static'],
-      },
-    },
-    build: {
-      sourcemap: true,
-    },
-  };
+export default defineConfig({
+  plugins: [sveltekit()],
 });
