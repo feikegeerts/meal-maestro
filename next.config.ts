2d1951e5eaeb611ebed366b3004d94781a74928f
Feike Geerts <feike.geerts@storiogroup.com>
2025-07-25 22:09:48 +0200

    Revert "feat: add Supabase configuration and update environment handling"
    This reverts commit c29ce5590f50f06ca00f2ba7fe104171b3063dd3.

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/a/**",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
       },
     ],
-  },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
 };
 
