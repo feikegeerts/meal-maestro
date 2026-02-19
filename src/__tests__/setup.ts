e84d0132a26915091a6c8257eb6f18b502b456d8
Feike Geerts <feikegeerts@gmail.com>
2026-02-19 16:24:34 +0100

    Suppress console log, warn, and error outputs in test setup

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom/vitest";
+import { beforeEach, vi } from "vitest";
+
+beforeEach(() => {
+  vi.spyOn(console, "log").mockImplementation(() => {});
+  vi.spyOn(console, "warn").mockImplementation(() => {});
+  vi.spyOn(console, "error").mockImplementation(() => {});
+});
